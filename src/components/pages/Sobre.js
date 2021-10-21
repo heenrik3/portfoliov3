4253bf54c9f576da845ceb03cc11a62cbd909706
henrik3 <henriqlh@gmail.com>
2021-10-21 13:05:27 -0300

    sobre update #3

--- src/components/pages/Sobre.js
+++ src/components/pages/Sobre.js
@@ -1,11 +1,11 @@
-const bloco1 = 'olá! ME CHAMO LUIS HENRIQUE.'
-const bloco2 = 'SOU GRADUANDO EM ANÁLISE E DESENVOLVIMENTO DE SISTEMAS PELA UEZO.'
-const bloco3 = 'SEMPRE CURIOSO SOBRE TECNOLOGIA, DECIDI MERGULHAR NESSE MUNDO.'
-const bloco4 = 'TODO DIA TENTO ADQUIRIR UM POUCO MAIS DE CONHECIMENTO PORQUE EU ACREDITO QUE SABEDORIA, É A CHAVE FUNDAMENTAL NO DESENVOLVIMENTO HUMANO E PODE NOS REVELAR AS VERDADES OCULTAS.'
-const bloco5 = 'Atualmente, estudo o framework React.  Pretendo trabalhar com o desenvolvimento.' 
-const bloco6 = 'No tempo livre, gosto de brincar com desenvolvimento de webApps'
-
 function Sobre() {
+  const bloco1 = 'olá! ME CHAMO LUIS HENRIQUE.'
+  const bloco2 = 'SOU GRADUANDO EM ANÁLISE E DESENVOLVIMENTO DE SISTEMAS PELA UEZO.'
+  const bloco3 = 'SEMPRE CURIOSO SOBRE TECNOLOGIA, DECIDI MERGULHAR NESSE MUNDO.'
+  const bloco4 = 'TODO DIA TENTO ADQUIRIR UM POUCO MAIS DE CONHECIMENTO PORQUE EU ACREDITO QUE SABEDORIA, É A CHAVE FUNDAMENTAL NO DESENVOLVIMENTO HUMANO E PODE NOS REVELAR AS VERDADES OCULTAS.'
+  const bloco5 = 'Atualmente, estudo o framework React. Pretendo trabalhar com o desenvolvimento.'
+  const bloco6 = 'No tempo livre, gosto de brincar com desenvolvimento de webApps'
+
   return (
     <section className='sobre page'>
       <div className='section_descriptor'>sobre</div>
